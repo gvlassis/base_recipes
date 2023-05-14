69e90926cf655947216da0133ebba693913a03e4
Georgios Vlassis <gvlassis@mailbox.org>
2023-05-14 13:14:53 +0200

    Added nnn recipe

--- catimg/build.sh
+++ catimg/build.sh
@@ -1,3 +1,5 @@
 cmake "${SRC_DIR}" 
-make                 
+make
+
+mkdir -p "${PREFIX}/bin/"
 cp "${SRC_DIR}/bin/catimg" "${PREFIX}/bin/"
---
+++ nnn/build.sh
@@ -0,0 +1,4 @@
+make O_NERD=1 O_NOSSN=1 O_GITSTATUS=1 O_NAMEFIRST=1
+
+mkdir -p "${PREFIX}/bin/"
+cp "${SRC_DIR}/nnn" "${PREFIX}/bin/"
