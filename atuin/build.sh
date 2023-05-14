a8a84cd67e9f14087f08fe938ca3defb75b4c88d
Georgios Vlassis <gvlassis@mailbox.org>
2023-05-15 01:36:27 +0200

    Atuin recipe fixes

--- atuin/build.sh
+++ atuin/build.sh
@@ -1,4 +1,4 @@
-cargo install --path "${SRC_DIR}/atuin" --root ${PREFIX}
+cargo install --path "${SRC_DIR}/atuin" --root "${SRC_DIR}"
 
 mkdir -p "${PREFIX}/bin/"
 cp "${SRC_DIR}/bin/atuin" "${PREFIX}/bin/"
