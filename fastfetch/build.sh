cd2a38f480285eb8fec88867dd85befa8afdda5d
Georgios Vlassis <gvlassis@mailbox.org>
2023-05-16 16:13:46 +0200

    Transitioned to using system tools (speed+robustness)

--- fastfetch/build.sh
+++ fastfetch/build.sh
@@ -1,5 +1,36 @@
-cmake "${SRC_DIR}" 
-make
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: © 2023 Project's authors 
+# SPDX-License-Identifier: MIT
+function main(){
+    function check_dependencies(){
+        local dependencies=$@
+        readonly dependencies 
 
-mkdir -p "${PREFIX}/bin"
-cp "${SRC_DIR}/fastfetch" "${PREFIX}/bin"
+        local missing_dependencies=""
+        for dependency in $dependencies; do
+            if ! command -v "${dependency}" &>/dev/null; then
+                if [ -n "${missing_dependencies}" ]; then
+                    missing_dependencies="${missing_dependencies}, ${dependency}"
+                else
+                    missing_dependencies="${dependency}"
+                fi
+            fi
+        done
+
+        if [ -n "${missing_dependencies}" ]; then
+            local script_path="$(readlink -f "${0}")"
+            readonly script_path
+            printf "\e[31;1m${script_path} (Error): The following dependencies are not in PATH: ${missing_dependencies}.\e[0m\n" >&2
+            exit 1
+        fi
+    }
+    check_dependencies cmake make
+
+    cmake "${SRC_DIR}" 
+    make
+
+    mkdir -p "${PREFIX}/bin"
+    cp "${SRC_DIR}/fastfetch" "${PREFIX}/bin"
+}
+
+main
--- nnn/build.sh
+++ nnn/build.sh
@@ -1,4 +1,36 @@
-make O_NORL=1 O_NOMOUSE=1 O_NOBATCH=1 O_NOFIFO=1 O_NERD=1 O_NOSSN=1 O_NAMEFIRST=1
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: © 2023 Project's authors 
+# SPDX-License-Identifier: MIT
+function main(){
 
-mkdir -p "${PREFIX}/bin"
-cp "${SRC_DIR}/nnn" "${PREFIX}/bin"
+    function check_dependencies(){
+        local dependencies=$@
+        readonly dependencies 
+
+        local missing_dependencies=""
+        for dependency in $dependencies; do
+            if ! command -v "${dependency}" &>/dev/null; then
+                if [ -n "${missing_dependencies}" ]; then
+                    missing_dependencies="${missing_dependencies}, ${dependency}"
+                else
+                    missing_dependencies="${dependency}"
+                fi
+            fi
+        done
+
+        if [ -n "${missing_dependencies}" ]; then
+            local script_path="$(readlink -f "${0}")"
+            readonly script_path
+            printf "\e[31;1m${script_path} (Error): The following dependencies are not in PATH: ${missing_dependencies}.\e[0m\n" >&2
+            exit 1
+        fi
+    }
+    check_dependencies make
+
+    make O_NORL=1 O_NOMOUSE=1 O_NOBATCH=1 O_NOFIFO=1 O_NERD=1 O_NOSSN=1 O_NAMEFIRST=1
+
+    mkdir -p "${PREFIX}/bin"
+    cp "${SRC_DIR}/nnn" "${PREFIX}/bin"
+}
+
+main
